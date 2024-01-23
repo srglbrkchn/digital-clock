77ddd868ba746bd86259830ef01da27aac45121b
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-01-23 14:29:51 +0100

    add day and the date to the clock. final commit.

--- app.py
+++ app.py
@@ -3,12 +3,24 @@ from time import *
 
 def update():
     time_string = strftime("%I:%M:%S %p")
-    time_lable.config(text=time_string)
-    time_lable.after(1000, update)
+    time_label.config(text=time_string)
+    window.after(1000, update)
+
+    day_string = strftime("%A")
+    day_label.config(text=day_string)
+
+    date_string = strftime("%B %d, %Y")
+    date_label.config(text=date_string)
 window = Tk()
 
-time_lable = Label(window, font=("Roboto-Black", 50), fg="#00FF00", bg="black")
-time_lable.pack()
+time_label = Label(window, font=("Roboto-Black", 50), fg="#00FF00", bg="black")
+time_label.pack()
+
+day_label= Label(window, font=("Roboto-Black", 28))
+day_label.pack()
+
+date_label = Label(window, font=("Roboto-Black", 30))
+date_label.pack()
 
 update()
 
